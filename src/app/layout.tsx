69b1ff3a51fb38e3752dca4a615c12753e6c5e12
shivambudhia <shivam@thesys.dev>
2025-07-03 11:47:02 +0530

    added theme and font config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,100..900;1,100..900&family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Figtree:ital,wght@0,300..900;1,300..900&family=Geist+Mono:wght@100..900&family=Geist:wght@100..900&family=Host+Grotesk:ital,wght@0,300..800;1,300..800&family=IBM+Plex+Serif:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Manrope:wght@200..800&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+          precedence="default"
+        />
+      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,56 @@
 
 import { C1Chat } from "@thesysai/genui-sdk";
 import "@crayonai/react-ui/styles/index.css";
+import { Theme, themePresets } from "@crayonai/react-ui";
+
+export type ThemeFont =
+  | 'Inter'
+  | 'Roboto'
+  | 'Plus Jakarta Sans'
+  | 'Open Sans'
+  | 'Bitter'
+  | 'Merriweather'
+  | 'Playfair Display'
+  | 'Crimson Text'
+  | 'Geist'
+  | 'Figtree'
+  | 'Manrope'
+  | 'Work Sans'
+  | 'DM Sans'
+  | 'IBM Plex Serif'
+  | 'Space Mono'
+  | 'Geist Mono'
+  | 'Host Grotesk'
+
+const generateTypography = () => ({
+  fontPrimary: `400 16px/20px __FONT__`,
+  fontHeadingLarge: `600 16px/20px __FONT__`,
+  fontHeadingMedium: `600 16px/20px __FONT__`,
+  fontHeadingSmall: `600 16px/20px __FONT__`,
+  fontTitle: `500 16px/20px __FONT__`,
+  fontTitleMedium: `500 16px/20px __FONT__`,
+  fontTitleSmall: `500 16px/20px __FONT__`,
+  fontBody: `400 16px/20px __FONT__`,
+  fontBodyMedium: `400 16px/20px __FONT__`,
+  fontBodySmall: `400 16px/20px __FONT__`,
+  fontBodyHeavy: `600 16px/20px __FONT__`,
+  fontBodySmallHeavy: `600 16px/20px __FONT__`,
+  fontBodyLink: `500 16px/20px __FONT__`,
+  fontLabelLarge: `400 16px/20px __FONT__`,
+  fontLabelLargeHeavy: `500 16px/20px __FONT__`,
+  fontLabel: `400 16px/20px __FONT__`,
+  fontLabelHeavy: `500 16px/20px __FONT__`,
+  fontLabelMedium: `400 16px/20px __FONT__`,
+  fontLabelMediumHeavy: `600 16px/20px __FONT__`,
+  fontLabelSmall: `400 16px/20px __FONT__`,
+  fontLabelSmallHeavy: `500 16px/20px __FONT__`,
+  fontLabelExtraSmall: `400 16px/20px __FONT__`,
+  fontLabelExtraSmallHeavy: `500 16px/20px __FONT__`,
+})
 
 export default function Home() {
-  return <C1Chat apiUrl="/api/chat" theme={{ mode: "dark" }} />;
+  return <C1Chat 
+    apiUrl="/api/chat"
+    theme={{ theme: `__THEME_CONFIG__` as Theme, ...generateTypography() }}
+  />;
 }
