b20043c2fc9fa259a4e25f8d6387d5247b0eae96
zahlekhan <zahle@thesys.dev>
2025-08-21 09:34:39 +0530

    replace c1 nightly

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: NextRequest) {
   messageStore.addMessage(prompt);
 
   const llmStream = await client.chat.completions.create({
-    model: "c1-nightly",
+    model: "c1/anthropic/claude-sonnet-4/v-20250815",
     messages: messageStore.getOpenAICompatibleMessageList(),
     stream: true,
   });
