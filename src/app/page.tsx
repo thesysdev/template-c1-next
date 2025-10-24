d6b5dbb03df710e119ddf7b8ea53e362bf4c7266
shivambudhia <shivam@thesys.dev>
2025-10-24 15:25:07 +0530

    theme updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,30 @@
 
 import { C1Chat, ThemeProvider } from "@thesysai/genui-sdk";
 import "@crayonai/react-ui/styles/index.css";
-import { Theme } from "@crayonai/react-ui";
 import clsx from "clsx";
 import styles from "./page.module.scss";
 import { useTheme } from "@crayonai/react-ui/ThemeProvider";
+import { theme, darkTheme, themeMode } from "@/theme";
 
-const theme = __THEME_CONFIG__ as Theme;
+// const ChatInternal = () => {
+//   const { portalThemeClassName } = useTheme();
 
-const ChatInternal = () => {
+//   return (
+//     <>
+//       <style>{__CUSTOM_THEME_STYLE__}</style>
+//       <C1Chat apiUrl="/api/chat" />
+//     </>
+//   );
+// };
+
+export default function Home() {
   const { portalThemeClassName } = useTheme();
 
   return (
-    <>
-      <style>{__CUSTOM_THEME_STYLE__}</style>
-      <C1Chat apiUrl="/api/chat" />
-    </>
-  );
-};
-
-export default function Home() {
-  return (
     <div className={clsx("!h-full !w-full", styles["chat-theme"])}>
-      <ThemeProvider theme={theme}>
-        <ChatInternal />
+      <ThemeProvider theme={theme} darkTheme={darkTheme} mode={themeMode}>
+        <style>{__CUSTOM_THEME_STYLE__}</style>
+        <C1Chat apiUrl="/api/chat" />
       </ThemeProvider>
     </div>
   );
